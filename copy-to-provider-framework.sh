d5692561ddd6171c3ce0ab8433b68c69e61510a5
David Bosschaert <david.bosschaert@gmail.com>
2012-06-27 21:46:33 +0100

    Script to copy to target VM

---
+++ copy-to-provider-framework.sh
@@ -0,0 +1,24 @@
+VERSION=1.0.0-SNAPSHOT
+TARGET=$1
+
+function copy {
+  MODULE="$1"
+  BUNDLE="$MODULE-$VERSION.jar"
+  cp $MODULE/target/$BUNDLE $TARGET/osgi/equinox/bundles
+  echo "bundles/$BUNDLE@start, \\" >> $TARGET/osgi/equinox/config/config.ini  
+}
+
+if [ ! -e $TARGET/osgi/equinox ] 
+then
+    echo Directory structure not as expected in $TARGET
+    echo Expected at least an osgi/equinox directory in that location
+    exit
+fi
+
+cp $TARGET/osgi/equinox/config-template/config.ini $TARGET/osgi/equinox/config/config.ini
+
+copy cloud-disco-demo-api
+copy cloud-disco-demo-provider
+
+echo " " >> $TARGET/osgi/equinox/config/config.ini  
+
