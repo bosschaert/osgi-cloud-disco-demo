a4ec3d0588efdc32f0394d2785448ffdd5b5f927
David Bosschaert <david.bosschaert@gmail.com>
2013-03-21 14:10:28 +0000

    wip

--- cloud-disco-demo-provider/src/main/java/org/coderthoughts/cloud/demo/provider/impl/Activator.java
+++ cloud-disco-demo-provider/src/main/java/org/coderthoughts/cloud/demo/provider/impl/Activator.java
@@ -1,29 +1,36 @@
 package org.coderthoughts.cloud.demo.provider.impl;
 
-import java.util.Dictionary;
-import java.util.Hashtable;
-
-import org.apache.cxf.dosgi.dsw.RemoteServiceFactory;
-import org.coderthoughts.cloud.demo.api.LongRunningService;
-import org.coderthoughts.cloud.demo.api.TestService;
-import org.coderthoughts.cloud.framework.service.api.FrameworkStatus;
+import org.coderthoughts.cloud.framework.service.api.FrameworkMetadataPublisher;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.Constants;
-import org.osgi.framework.ServiceRegistration;
-import org.osgi.service.monitor.Monitorable;
+import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
 
 public class Activator implements BundleActivator {
-    private ServiceRegistration lrsReg;
-    private ServiceRegistration tsReg;
-    private ServiceRegistration mReg;
+//    private ServiceRegistration lrsReg;
+//    private ServiceRegistration tsReg;
+//    private ServiceRegistration mReg;
 
     @Override
     public void start(BundleContext context) throws Exception {
+        ServiceTracker st = new ServiceTracker(context, FrameworkMetadataPublisher.class.getName(), null) {
+            @Override
+            public Object addingService(ServiceReference reference) {
+                Object svc = super.addingService(reference);
+                if (svc instanceof FrameworkMetadataPublisher) {
+                    ((FrameworkMetadataPublisher) svc).addProperty("org.coderthoughts.application.role", "database");
+                    System.out.println("Updated metadata");
+                }
+                return svc;
+            }
+        };
+        st.open();
+
+        /*
         TestService ts = new TestServiceImpl(context.getProperty("org.osgi.framework.uuid"));
         Dictionary<String, Object> tsProps = new Hashtable<String, Object>();
         tsProps.put("service.exported.interfaces", "*");
-        tsProps.put("service.exported.configs", new String [] {"org.coderthoughts.configtype.cloud", "<<nodefault>>"});
+        tsProps.put("service.exported.configs", new String [] {CloudConstants.CLOUD_CONFIGURATION_TYPE, "<<nodefault>>"});
         TestServiceRSF tsControl = new TestServiceRSF(context);
         tsProps.put("org.coderthoughts.remote.service.factory", tsControl);
         tsReg = context.registerService(TestService.class.getName(), ts, tsProps);
@@ -38,16 +45,17 @@ public class Activator implements BundleActivator {
         LongRunningService lrs = new LongRunningServiceImpl();
         Dictionary<String, Object> lrsProps = new Hashtable<String, Object>();
         lrsProps.put("service.exported.interfaces", "*");
-        lrsProps.put("service.exported.configs", new String [] {"org.coderthoughts.configtype.cloud", "<<nodefault>>"});
+        lrsProps.put("service.exported.configs", new String [] {CloudConstants.CLOUD_CONFIGURATION_TYPE, "<<nodefault>>"});
         RemoteServiceFactory lrsFactory = new LongRunningServiceRSF();
         lrsProps.put("org.coderthoughts.remote.service.factory", lrsFactory);
         lrsReg = context.registerService(LongRunningService.class.getName(), lrs, lrsProps);
+        */
     }
 
     @Override
     public void stop(BundleContext context) throws Exception {
-        lrsReg.unregister();
-        tsReg.unregister();
-        mReg.unregister();
+//        lrsReg.unregister();
+//        tsReg.unregister();
+//        mReg.unregister();
     }
 }
